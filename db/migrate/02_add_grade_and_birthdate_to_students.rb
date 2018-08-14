2cbb8fe59b9c9c326d2589931f0fa750f7934366
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-14 19:32:32 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 
 end
